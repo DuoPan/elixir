8e8ff950f2614cc96bbf08bd64e4018ed72fc634
duo <pd54007@gmail.com>
2020-09-26 12:14:00 +1000

    3/94

--- exercises/anagram/lib/anagram.ex
+++ exercises/anagram/lib/anagram.ex
@@ -4,5 +4,22 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    Enum.filter(candidates, fn x -> ma(x, base) end)
+  end
+
+  def ma(a, b) do 
+    x = a
+    |> String.downcase()
+    |> String.to_charlist()
+
+    y = b
+    |> String.downcase()
+    |> String.to_charlist()
+
+    if x == y do
+      false
+    else
+      Enum.sort(x) == Enum.sort(y) 
+    end
   end
 end
