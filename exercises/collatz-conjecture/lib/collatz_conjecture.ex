46c6a73a5e00eb4099994ab0e1feaa7cf6c64df2
duo <pd54007@gmail.com>
2020-09-27 19:35:12 +1000

    Easy

--- exercises/collatz-conjecture/lib/collatz_conjecture.ex
+++ exercises/collatz-conjecture/lib/collatz_conjecture.ex
@@ -7,5 +7,18 @@ defmodule CollatzConjecture do
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
   def calc(input) do
+    calc(input, 0)
+  end
+
+  def calc(input, number) when is_integer(input) do
+    cond do
+      input <= 0 -> raise FunctionClauseError
+      input == 1 -> number
+      rem(input, 2) == 1 -> calc(input*3+1, number+1)
+      rem(input, 2) == 0 -> calc(div(input, 2), number+1)
+    end
+  end
+  def calc(_input, _number) do
+    raise FunctionClauseError
   end
 end
