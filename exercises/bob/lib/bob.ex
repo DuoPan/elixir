bdf1c5dfaa1df182c237c9d531841e27dbd5d5be
duo <pd54007@gmail.com>
2020-09-27 13:58:47 +1000

    Learn to split methods and use Regex.

--- exercises/bob/lib/bob.ex
+++ exercises/bob/lib/bob.ex
@@ -1,7 +1,18 @@
 defmodule Bob do
   def hey(input) do
+    input = String.trim(input)
+    containQuestionMark = input |> String.graphemes() |> Enum.at(-1) == "?"
+    shout = input =~ ~r(^[^a-z]*$)
+    onlyNumbers = input =~ ~r(^[^a-zA-Z]*$)
     cond do
-      true -> raise "Your implementation goes here"
+      String.trim(input) == "" -> "Fine. Be that way!"
+      onlyNumbers && containQuestionMark -> "Sure."
+      onlyNumbers -> "Whatever."
+      !containQuestionMark && shout -> "Whoa, chill out!"
+      containQuestionMark && shout  -> "Calm down, I know what I'm doing!"
+      containQuestionMark -> "Sure."
+      input -> "Whatever."
     end
   end
 end
+# Enum.all?(String.graphemes(input), fn x -> )
