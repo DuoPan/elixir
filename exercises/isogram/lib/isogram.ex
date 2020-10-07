e96388f242c383c1f45935570ed2f42394171fc6
Duo Pan <pd54007@gmail.com>
2020-10-07 22:03:10 +1100

    First running in Windows.

--- exercises/isogram/lib/isogram.ex
+++ exercises/isogram/lib/isogram.ex
@@ -1,8 +1,14 @@
 defmodule Isogram do
   @doc """
   Determines if a word or sentence is an isogram
+  isogram: no letters occurs more than once.
   """
   @spec isogram?(String.t()) :: boolean
   def isogram?(sentence) do
+    list = sentence
+    |> String.graphemes()
+    |> Enum.filter(fn x -> x != "-" && x != " " end)
+
+    length(Enum.uniq(list)) == length(list)
   end
 end
